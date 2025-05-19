4c6adcf6e870e45150487020069791acdeb1bc8f
shaun <shaun.whalen-smith@pmconnect.co.uk>
2025-05-19 11:00:14 +0100

    fix: implement debouncing on the search so user can type freely, without interrupted typing on searches. Also added Input adornment to show a search icon.

--- src/components/filters/CrimeSearchFilter.jsx
+++ src/components/filters/CrimeSearchFilter.jsx
@@ -1,14 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { TextField } from '@mui/material';
+import SearchIcon from '@mui/icons-material/Search';
+import InputAdornment from '@mui/material/InputAdornment';
 
+// Search filter text fieldcomponent for the crimes table
 export function CrimeSearchFilter({ search, setSearch }) {
+  const [localSearch, setLocalSearch] = useState(search);
+
+  // Debounce the search input, will only trigger after 300ms of inactivity in user typing
+  useEffect(() => {
+    const handler = setTimeout(() => {
+      setSearch(localSearch);
+    }, 300);
+    return () => clearTimeout(handler); // reset the timeout if user carrys on typing
+  }, [localSearch, setSearch]);
+
   return (
     <TextField
-      label="Search crimes, (filtered as you type)"
+      label="Search crimes"
       variant="outlined"
       className="crime-search-filter"
-      value={search}
-      onChange={e => setSearch(e.target.value)}
+      value={localSearch}
+      onChange={e => setLocalSearch(e.target.value)}
+      slotProps={{
+        input: {
+          startAdornment: (
+            <InputAdornment position="start">
+              <SearchIcon color="action" />
+            </InputAdornment>
+          ),
+        },
+      }}
     />
   );
 }
