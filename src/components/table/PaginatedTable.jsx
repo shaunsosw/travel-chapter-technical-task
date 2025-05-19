076788d5a24be321bd9f6eca24c56b8bb5e91129
shaun <shaun.whalen-smith@pmconnect.co.uk>
2025-05-19 15:22:14 +0100

    fix: Change to use 10 rows per page by default.

--- src/components/table/PaginatedTable.jsx
+++ src/components/table/PaginatedTable.jsx
@@ -3,7 +3,7 @@ import { TablePagination } from '@mui/material';
 import { usePagination } from '../../hooks/usePagination';
 
 // Wrapper component for the table that adds pagination returns the paginated data and the pagination controls
-export function PaginatedTable({ data, rowsPerPageOptions = [5, 10, 25, 50], defaultRowsPerPage = 15, children }) {
+export function PaginatedTable({ data, rowsPerPageOptions = [5, 10, 25, 50], defaultRowsPerPage = 10, children }) {
   const {
     page,
     rowsPerPage,
